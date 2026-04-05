325af8674b3bad31b32c9e22ed84c6c08c47ee84
MarsiSomeone <sirlovaolga@gmail.com>
2026-04-06 01:53:51 +0300

    patch 1 hello world update comment

--- lb2/hello_world.cpp
+++ lb2/hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
-int main(){
 
-std::string n;
-std::cout<<"Enter name: ";
-std::cin >> n;
-std::cout<<std::endl<<"Hello world from "<<n<<std::endl;
+//main function
+int main(){
+//world execute.(me)
+std::string n;//variabre
+std::cout<<"Enter name: ";//output comment
+std::cin >> n;//input function
+std::cout<<std::endl<<"Hello world from "<<n<<std::endl;//final output
  return 0;}
